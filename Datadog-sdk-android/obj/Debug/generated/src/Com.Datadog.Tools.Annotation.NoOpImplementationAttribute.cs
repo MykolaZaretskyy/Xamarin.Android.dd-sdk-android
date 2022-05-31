d0fcbea8e45879c450dc9775bbc409cae1c1333c
mykola.zaretskyy <mzaretskyy@oneviewhealthcare.com>
2022-05-31 15:48:27 +0200

    remove bin/obg folders

--- Datadog-sdk-android/obj/Debug/generated/src/Com.Datadog.Tools.Annotation.NoOpImplementationAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Com.Datadog.Tools.Annotation {
-
-	[global::Android.Runtime.Annotation ("com.datadog.tools.annotation.NoOpImplementation")]
-	public partial class NoOpImplementationAttribute : Attribute
-	{
-	}
-}
